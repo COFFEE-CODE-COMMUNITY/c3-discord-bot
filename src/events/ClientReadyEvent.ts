2c8640cf28a46109f3c0fa46c07f9277f907b850
MuhammadFauzi27 <fauzym02@gmail.com>
2025-06-02 16:00:13 +0700

    add invite-tracker branch and pre-feature

--- src/events/ClientReadyEvent.ts
+++ src/events/ClientReadyEvent.ts
@@ -1,8 +1,13 @@
 import DiscordEventListener from "../abstracts/DiscordEventListener"
-import { Client, Events } from "discord.js"
+import { Client, Events, Collection, Invite, Guild } from "discord.js"
 import Logger from "../infrastructures/Logger"
 import StatRoleService from "../services/StatRoleService"
+import config from "../infrastructures/config"
 import { injectable } from "inversify"
+
+//Global variable for invite tracker feature
+export const inviteCaches = new Map<string, Collection<string, Invite>>
+const guildId = config.get("c3.guild.id")
 
 @injectable()
 class ClientReadyEvent extends DiscordEventListener<Events.ClientReady> {
@@ -23,6 +28,21 @@ class ClientReadyEvent extends DiscordEventListener<Events.ClientReady> {
       this.logger.info("Stats Server Ready")
     } catch (error) {
       this.logger.error(`Failed to start stats server ${error}`)
+    }
+
+    //Invite tracker logic
+    const guild = client.guilds.cache.get(guildId)
+    if (!guild) {
+      this.logger.error(`[InviteTracker] Guild with ID ${guildId} not found.`)
+      return
+    }
+
+    try {
+      const invites = await guild.invites.fetch()
+      inviteCaches.set(guildId, invites)
+      this.logger.info(`[InviteTracker] Cached invites for guild "${guild.name}"`)
+    } catch(error) {
+      this.logger.warn(`[InviteTracker] Could not fetch invites for guild "${guild.name}": ${error}`)
     }
   }
 }
